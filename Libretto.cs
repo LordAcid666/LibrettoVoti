228d52ae0c028e756b5c6893e25323c21b46579d
Giacomo <gc76@live.it>
2022-03-11 21:58:36 +0100

    add Libretto constructor

--- Libretto.cs
+++ Libretto.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using System.Collections.Generic;
 
 namespace LibrettoVoti
@@ -5,5 +6,10 @@ namespace LibrettoVoti
     public class Libretto
     {
         private List<Voto> voti;
+
+        public Libretto()
+        {
+            this.voti = new List<Voto>();
+        } 
     }
 }
