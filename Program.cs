682dff4e62e5cd7dc62e3ac00b0930e19b1e96d2
Giacomo <gc76@live.it>
2022-03-11 21:45:59 +0100

    add .gitignore

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LibrettoVoti
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
