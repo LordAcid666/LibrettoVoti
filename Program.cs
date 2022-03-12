dc09ec9ea5f8eff12de066a4fc0c2c6711dc9176
Giacomo <gc76@live.it>
2022-03-12 11:55:41 +0100

    add materie

--- Program.cs
+++ Program.cs
@@ -9,7 +9,15 @@ namespace LibrettoVoti
             Libretto lib = new Libretto();
             lib.add(new Voto("Analisi 1", 30));
             lib.add(new Voto("Fisica 1", 18));
+            lib.add(new Voto("Informatica", 25));
+            lib.add(new Voto("Algebra 1", 25));
+
             Console.WriteLine(lib);
+            Console.WriteLine("Voti pari a 25: ");
+
+            Libretto lib25 = lib.filtraPunti(25);
+            Console.WriteLine(lib25);
+
         }
     }
 }
